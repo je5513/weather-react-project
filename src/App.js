9bd05b654b3f6b479bd86f3810ff5f3b1a76e3b4
je5513 <jessica.shore@hotmail.co.uk>
2025-03-09 13:51:04 +0000

    Added github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Jessica Shore and is
+        <a
+          href="https://github.com/je5513/weather-react-project"
+          target="_blank"
+        >
+          {" "}
+          open sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
