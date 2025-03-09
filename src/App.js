4af601b37de3ede81c3c98cef4c36db55829dd05
je5513 <jessica.shore@hotmail.co.uk>
2025-03-09 14:03:45 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Jessica Shore and is
-        <a
-          href="https://github.com/je5513/weather-react-project"
-          target="_blank"
-        >
-          {" "}
-          open sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Jessica Shore and is
+          <a
+            href="https://github.com/je5513/weather-react-project"
+            target="_blank"
+          >
+            {" "}
+            open sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
